a3704f13e52b0c9136fb55ab3f8495dec2f07258
mohsin6862 <mohsinctg2206@gmail.com>
2023-04-27 23:08:56 +0600

    Catagory data added

--- src/Pages/LeftNav/LeftNav.jsx
+++ src/Pages/LeftNav/LeftNav.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+
+    const [catagories, setCatagories] = useState([]);
+
+    useEffect(()=>{
+        fetch('catagoryData.js')
+        .then(res => res.json())
+        .then(data => setCatagories(data))
+    },[])
+
     return (
         <div>
-            <h3>Left Nav</h3>
+            <h3>All Catagory</h3>
+            <div>
+                {
+                    catagories.map(catagory => <Link> catagory.name</Link>)
+                }
+            </div>
         </div>
     );
 };
