6c3ec876c1c89d55f8ae01761f55fcb006f45ffa
mohsin6862 <mohsinctg2206@gmail.com>
2023-04-27 18:42:57 +0600

    Header added

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import logo from '../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h1>This  is Header</h1>
+           <div className='text-center my-5'>
+           <img src={logo} alt="" />
+            <h6 className='mt-2'>Journalism Without Fear or Favour</h6>
+            <p >{moment().format("dddd, MMMM D, YYYY")}</p>
+           </div>
         </div>
     );
 };
