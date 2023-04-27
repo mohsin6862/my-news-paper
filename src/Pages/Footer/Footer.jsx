5617b11c348123c6f047b27555373ea4987bafe9
mohsin6862 <mohsinctg2206@gmail.com>
2023-04-27 18:12:20 +0600

    Route setup

---
+++ src/Pages/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>All Copyright reserved for Dregon News.com</h1>
+        </div>
+    );
+};
+
+export default Footer;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Main = () => {
+const Home = () => {
     return (
         <div>
-            
+          <h1>This is home</h1>  
         </div>
     );
 };
 
-export default Main;
+export default Home;
